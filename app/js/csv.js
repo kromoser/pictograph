e1447270bae7b60813cd95ad4385dce1e4035d1c
kromoser <kevinromoser@gmail.com>
2019-02-10 14:35:10 -0500

    Add percentages adjusted to values between 1 and 10

--- app/js/csv.js
+++ app/js/csv.js
@@ -12,11 +12,20 @@ function createList() {
 
     const columnOne = data.columns[0];
     const columnTwo = data.columns[1];
+
+
+    // Get total counts from column two for use in percentages
+    const reducer = (accumulator, currentValue) => accumulator + parseInt(currentValue[columnTwo])
+    const totalCount = data.reduce(reducer,0);
+
     data.forEach(function(element) {
       const node = document.createElement('LI');
-      const text = document.createTextNode(element[columnOne] + ': ' + element[columnTwo]);
+      // Get each value to be normalized to a distribution between 1 and 10
+      const text = document.createTextNode(element[columnOne] + ': ' + (parseInt(element[columnTwo])/totalCount * 10) + ' (Totals:' + element[columnTwo] + '/' + totalCount + ')');
       node.appendChild(text);
       graphContainer.appendChild(node)
     });
+
+    console.log(data.reduce(reducer,0));
   });
 }
--- dist/all.js
+++ dist/all.js
@@ -6,13 +6,21 @@ function createList() {
   d3.csv('/app/csv/test.csv').then(function (data) {
     console.log(data);
     var columnOne = data.columns[0];
-    var columnTwo = data.columns[1];
+    var columnTwo = data.columns[1]; // Get total counts from column two for use in percentages
+
+    var reducer = function reducer(accumulator, currentValue) {
+      return accumulator + parseInt(currentValue[columnTwo]);
+    };
+
+    var totalCount = data.reduce(reducer, 0);
     data.forEach(function (element) {
-      var node = document.createElement('LI');
-      var text = document.createTextNode(element[columnOne] + ': ' + element[columnTwo]);
+      var node = document.createElement('LI'); // Get each value to be normalized to a distribution between 1 and 10
+
+      var text = document.createTextNode(element[columnOne] + ': ' + parseInt(element[columnTwo]) / totalCount * 10 + ' (Totals:' + element[columnTwo] + '/' + totalCount + ')');
       node.appendChild(text);
       graphContainer.appendChild(node);
     });
+    console.log(data.reduce(reducer, 0));
   });
 }
 //# sourceMappingURL=all.js.map
