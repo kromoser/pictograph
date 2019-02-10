cb9207447d88ae9730ebc3ffb42c9bc1ce6dd11d
kromoser <kevinromoser@gmail.com>
2019-02-10 15:11:36 -0500

    Draw svg x times according to proportion of results of each row in csv.

--- app/js/csv.js
+++ app/js/csv.js
@@ -1,6 +1,7 @@
 
 
 
+
 function createList() {
 
   const graphContainer = document.querySelector('#vis-container');
@@ -21,11 +22,34 @@ function createList() {
     data.forEach(function(element) {
       const node = document.createElement('LI');
       // Get each value to be normalized to a distribution between 1 and 10
-      const text = document.createTextNode(element[columnOne] + ': ' + (parseInt(element[columnTwo])/totalCount * 10) + ' (Totals:' + element[columnTwo] + '/' + totalCount + ')');
-      node.appendChild(text);
+      const symbolTotal = Math.round(element[columnTwo]/totalCount * 10)
+      const label = document.createTextNode(element[columnOne] + ': ');
+      const totals = document.createTextNode('Totals:' + element[columnTwo] + '/' + totalCount + ')')
+
+      node.appendChild(label);
+
+      console.log(symbolTotal)
+
+      for ( let i = 0; i < symbolTotal; i++ ) {
+        drawPersonSVG(node)
+        //const symbol = document.createTextNode('<3')
+        //node.appendChild(symbol)
+      };
+
+      node.appendChild(totals)
       graphContainer.appendChild(node)
     });
 
     console.log(data.reduce(reducer,0));
   });
+}
+
+function drawPersonSVG(target) {
+    d3.select(target)
+      .append('svg')
+      .attr('width', 24)
+      .attr('height', 24)
+      .append('path')
+      .attr('d', 'M5,1C5,3.7 6.56,6.16 9,7.32V22H11V15H13V22H15V7.31C17.44,6.16 19,3.7 19,1H17A5,5 0 0,1 12,6A5,5 0 0,1 7,1M12,1C10.89,1 10,1.89 10,3C10,4.11 10.89,5 12,5C13.11,5 14,4.11 14,3C14,1.89 13.11,1 12,1Z')
+      .style('fill', 'purple')
 }
--- dist/all.js
+++ dist/all.js
@@ -16,11 +16,26 @@ function createList() {
     data.forEach(function (element) {
       var node = document.createElement('LI'); // Get each value to be normalized to a distribution between 1 and 10
 
-      var text = document.createTextNode(element[columnOne] + ': ' + parseInt(element[columnTwo]) / totalCount * 10 + ' (Totals:' + element[columnTwo] + '/' + totalCount + ')');
-      node.appendChild(text);
+      var symbolTotal = Math.round(element[columnTwo] / totalCount * 10);
+      var label = document.createTextNode(element[columnOne] + ': ');
+      var totals = document.createTextNode('Totals:' + element[columnTwo] + '/' + totalCount + ')');
+      node.appendChild(label);
+      console.log(symbolTotal);
+
+      for (var i = 0; i < symbolTotal; i++) {
+        drawPersonSVG(node); //const symbol = document.createTextNode('<3')
+        //node.appendChild(symbol)
+      }
+
+      ;
+      node.appendChild(totals);
       graphContainer.appendChild(node);
     });
     console.log(data.reduce(reducer, 0));
   });
+}
+
+function drawPersonSVG(target) {
+  d3.select(target).append('svg').attr('width', 24).attr('height', 24).append('path').attr('d', 'M5,1C5,3.7 6.56,6.16 9,7.32V22H11V15H13V22H15V7.31C17.44,6.16 19,3.7 19,1H17A5,5 0 0,1 12,6A5,5 0 0,1 7,1M12,1C10.89,1 10,1.89 10,3C10,4.11 10.89,5 12,5C13.11,5 14,4.11 14,3C14,1.89 13.11,1 12,1Z').style('fill', 'purple');
 }
 //# sourceMappingURL=all.js.map
