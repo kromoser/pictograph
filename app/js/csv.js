0913797c92fa03afb96cee4da48667b83fb94ba0
kromoser <kevinromoser@gmail.com>
2019-02-10 12:53:21 -0500

    Stub out basic csv processing

--- app/js/csv.js
+++ app/js/csv.js
@@ -1,5 +1,22 @@
 
 
-d3.csv('/app/csv/test.csv').then(function(data) {
-  console.log(data)
-})
+
+function createList() {
+
+  const graphContainer = document.querySelector('#vis-container');
+
+  // Take CSV, split into row objects, and push them into array
+  d3.csv('/app/csv/test.csv').then(function(data) {
+
+    console.log(data);
+
+    const columnOne = data.columns[0];
+    const columnTwo = data.columns[1];
+    data.forEach(function(element) {
+      const node = document.createElement('LI');
+      const text = document.createTextNode(element[columnOne] + ': ' + element[columnTwo]);
+      node.appendChild(text);
+      graphContainer.appendChild(node)
+    });
+  });
+}
--- dist/all.js
+++ dist/all.js
@@ -1,6 +1,18 @@
 "use strict";
 
-d3.csv('/app/csv/test.csv').then(function (data) {
-  console.log(data);
-});
+function createList() {
+  var graphContainer = document.querySelector('#vis-container'); // Take CSV, split into row objects, and push them into array
+
+  d3.csv('/app/csv/test.csv').then(function (data) {
+    console.log(data);
+    var columnOne = data.columns[0];
+    var columnTwo = data.columns[1];
+    data.forEach(function (element) {
+      var node = document.createElement('LI');
+      var text = document.createTextNode(element[columnOne] + ': ' + element[columnTwo]);
+      node.appendChild(text);
+      graphContainer.appendChild(node);
+    });
+  });
+}
 //# sourceMappingURL=all.js.map
